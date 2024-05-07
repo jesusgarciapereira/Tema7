fde8452acb8f734a3150347125f25323be88bda0
Jesús <jesus.garciap@iesnervion.es>
2024-05-07 19:22:32 +0200

    Ejercicio 04 corregido

--- src/collections/Ejercicio04.java
+++ src/collections/Ejercicio04.java
@@ -5,42 +5,59 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Clase que representa el Ejercicio04
+ */
 public class Ejercicio04 {
 
 	public static void main(String[] args) {
+
+		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
 
+		// Cada uno de los nombres que le pediremos al usuario
+		String nombre = "";
+
+		// Creamos una lista de tipo String
 		List<String> nombres = new ArrayList<String>();
+
+		// Le pedimos al usuario que escriba 5 nombres
 		System.out.println("Escribe 5 nombres:");
 
-		/*
-		 * for (int i = 0; i < 5; i++) { System.out.println("Nombre " + (i + 1) + ": ");
-		 * String nombre = sc.nextLine(); if (nombres.contains(nombre)) { System.out.
-		 * println("Nombre ya incluido en el registro, introduzca otro distinto"); i--;
-		 * 
-		 * } else { nombres.add(nombre); }
-		 */
-
+		// Mientras el tamaño de la lista sea menor que 5
 		while (nombres.size() < 5) {
-
+			// Mostramos este texto
 			System.out.println("Nombre " + (nombres.size() + 1) + ": ");
-			String nombre = sc.nextLine();
-			if (nombres.contains(nombre)) {
-				System.out.println("Nombre ya incluido en el registro, introduzca otro distinto");
+			// Asignamos el texto introducido al usuario como nombre
+			nombre = sc.nextLine();
 
-			} else {
+			// Si la lista contiene ese nombre
+			if (nombres.contains(nombre))
+				// Sólo mostrará este texto
+				System.out.println("El nombre \"" + nombre
+						+ "\" ya está incluido en el registro, introduzca otro nombre distinto");
+			// En caso contrario
+			else
+				// Lo añadirá a la lista
 				nombres.add(nombre);
-			}
 
 		}
 
-		System.out.println("Nombre en orden inverso:");
+		// Mostraremos la lista en el orden inverso
+		System.out.println("Lista nombres en orden inverso:");
+
+		// Llamamos al método reverse() de la clase Collections para ordenarla al
+		// inverso
 		Collections.reverse(nombres);
-		for (String nombre : nombres) {
-			System.out.println(nombre);
+
+		// Bucle for-each para recorrer cada nombre de la lista nombres
+		for (String cadaNombre : nombres) {
+			// Mostramos cada nombre
+			System.out.println(cadaNombre);
 
 		}
-		
+
+		// Cerramos el Scanner
 		sc.close();
 	}
 }
